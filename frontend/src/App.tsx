35ed12e03d6a772fe675bf896431658932046d09
CCTC <CCTC@CC13635.COLLIN.EDU>
2025-11-08 01:32:22 -0600

    amy

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DebateStage from "./components/DebateStage";
+
+function App() {
+  return (
+    <div>
+      <DebateStage />
+    </div>
+  );
+}
+
+export default App;
